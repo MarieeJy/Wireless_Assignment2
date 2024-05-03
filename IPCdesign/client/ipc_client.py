4dcabe47b516f362ebc579281e0e6659ba8eef6d
Marie_Jiayu <denglele0402@163.com>
2024-05-03 17:54:28 +0100

    update design

--- IPCdesign/client/ipc_client.py
+++ IPCdesign/client/ipc_client.py
@@ -7,7 +7,7 @@ import random
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 9898        # The port used by the server
 
-numbers = ' '.join(str(random.random() * 100) for _ in range(50))
+numbers = ' '.join(str(random.random() * 100) for _ in range(50))   # generate a random number for testing
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
